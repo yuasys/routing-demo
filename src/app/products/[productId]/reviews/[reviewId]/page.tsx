3f46f12331c84049a214ab602e81f8cf5e8e3c8a
Nob Yuasys <yuasys@gmail.com>
2024-02-11 19:49:07 +0900

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,6 @@
+import { notFound } from "next/navigation";
+import { parse } from "path";
+
 export default function ReviewDetails({
    params,
    }: {
@@ -6,6 +9,9 @@ export default function ReviewDetails({
       reviewId: string ;
     };
     }) {
+      if (parseInt(params.reviewId) > 1000) {
+        notFound();
+      }
       return( 
       <h1>
           Review  {params.reviewId} for product {params.productId}
