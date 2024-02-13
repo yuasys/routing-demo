e07e1c3886fd4e09d7d6f7b0791da45825ad9ade
Nob Yuasys <yuasys@gmail.com>
2024-02-13 18:09:46 +0900

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import{Metadata} from 'next';
+
+export const metadata: Metadata = {
+  // title: "blog",  // この場合はlayout.tsxのtemplateが使われる
+  title: {
+    absolute: "Blog",// この場合はlayout.tsxのabsoluteが使われる
+  },
+};
+
 export default function Blog () {
   return <h1>My blog</h1>
 }
