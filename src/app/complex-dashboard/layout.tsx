64e279af74cb97169438c308e9507f1ea79c1d14
Nob Yuasys <yuasys@gmail.com>
2024-02-16 11:32:03 +0900

    コンプレックスダッシュボードに関連するファイルを追加しました

---
+++ src/app/complex-dashboard/layout.tsx
@@ -0,0 +1,25 @@
+export default function DashboardLayout({
+  children,
+  users,
+  revenue,
+  notifications,
+  }: { 
+    children: React.ReactNode;
+    users: React.ReactNode;
+    revenue: React.ReactNode;
+    notifications: React.ReactNode;
+  }
+) {
+  return (
+    <div>
+      <div>{children}</div>
+      <div style={{display:"flex"}}>
+        <div style={{display:"flex", flexDirection:"column"}}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div style={{display:"flex", flex: 1}}>{notifications}</div>
+      </div>
+    </div>
+  );
+}
